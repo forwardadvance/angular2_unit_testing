6647ea0e7876e51c0d78949cb98f8bcd98060362
Nicholas Johnson <nicholas@forwardadvance.com>
2017-03-23 11:57:49 +0000

    tidy up

--- demo/app/app.component.spec.ts
+++ demo/app/app.component.spec.ts
@@ -1,18 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-    import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 
-    describe('AppComponent', () => {
-      let component:    AppComponent;
-      let fixture: ComponentFixture<AppComponent>;
-      beforeEach(() => {
-        TestBed.configureTestingModule({
-          declarations: [AppComponent]
-        });
-        component = TestBed.createComponent(AppComponent).componentInstance;
-      });
+describe('AppComponent', () => {
 
-      it ('should work', () => {
-        expect(component instanceof AppComponent).toBe(true, 'should create AppComponent');
-      });
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
     });
+    component = TestBed.createComponent(AppComponent).componentInstance;
+  });
+
+  it ('should work', () => {
+    expect(component instanceof AppComponent).toBe(true, 'should create AppComponent');
+  });
+});
--- demo/app/specs.ts
+++ demo/app/specs.ts
@@ -9,6 +9,7 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
+
 import {TestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing'
 
